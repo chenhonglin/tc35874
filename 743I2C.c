8c7b2b88e08a5151358d7dff7535496bdce56f82
mhk411 <59460135+mhk411@users.noreply.github.com>
2020-08-04 13:52:05 +0900

    Update 743I2C.c

--- 743I2C.c
+++ 743I2C.c
@@ -14,33 +14,56 @@
 
 
 int main(){
-	int fd;
-	int err;
-	u8 *data;
-	data[0]=0x0002;
-	data[1]=0x0001;
-	struct i2c_msg i2c_msgs={
-		.addr = 0x0f,
-		.flags = 0,
-		.len = 2,
-		.buf = data,
-	};
 	
-	fd=open("/dev/i2c-6", O_RDWR);
+	int fd;
+	fd=open("/dev/i2c-6",O_RDWR);
 	
 	if(fd<0){
 		printf("open fail\n");
-		exit(1);
 	}
-	
-	err=ioctl(fd,I2C_RDWR,(struct i2c_rdwr_ioctl_data *) msgs);
-	
-	if(err<0){
-		printf("ioctl fail\n");
-	}
-	
+	/*write 함수*/
+     struct i2c_rdwr_ioctl_data msg_rdwr;
+     struct i2c_msg i2cmsg;
+     char my_buf[128];
 
-	
+     my_buf[0] = 0x0002;
+     my_buf[1] = 0x0001;
 
+     for(i= 0; i < len; i++)
+     {
+        my_buf[2+i] = buf[i];
+     }
+     msg_rdwr.msgs = &i2cmsg;
+     msg_rdwr.nmsgs = 1;
+     i2cmsg.addr  = I2C_ADDR;
+     i2cmsg.flags = 0;
+     i2cmsg.len   = 2+len;
+     i2cmsg.buf   = my_buf;
+
+    if(ioctl(fd,I2C_RDWR,&msg_rdwr)<0)
+    {
+          printf("write: %s\n", strerror(errno));
+          return -1;
+    }
+
+/*read함수*/
+    struct i2c_rdwr_ioctl_data msg_rdwr;
+    struct i2c_msg             i2cmsg;
+    
+    //위 write함수 후 (addr전송) 아래 read처리로 값 read
+
+    msg_rdwr.msgs = &i2cmsg;
+    msg_rdwr.nmsgs = 1;
+
+    i2cmsg.addr  = I2C_ADDR;
+    i2cmsg.flags = I2C_M_RD;
+    i2cmsg.len   = len;
+    i2cmsg.buf   = buf;
+
+    if(ioctl(fd,I2C_RDWR,&msg_rdwr)<0)
+    {
+        printf("read: %s\n", strerror(errno));
+        return -1;
+    }
 	return 0;
 }
